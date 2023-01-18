e505ea5135c6b4c31eff06598c425c9a2a0b5b75
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-18 21:36:46 +0900

    최소직사각형
    프로그래머스

---
+++ script.js
@@ -0,0 +1,26 @@
+let sizes = [[60, 50], [30, 70], [60, 30], [80, 40]];
+solution(sizes);
+
+function solution(sizes) {
+    var answer = 0;
+    
+    let left = 0;
+    let right = 0;
+    let maxLeft = 0;
+    let maxRight = 0;
+    
+    for(let i = 0; i < sizes.length; i++){
+        if(sizes[i][0] > sizes[i][1]){
+            left = sizes[i][0];
+            right = sizes[i][1];
+        }else {
+            left = sizes[i][1];
+            right = sizes[i][0];
+        }
+        maxLeft = Math.max(maxLeft, left);
+        maxRight = Math.max(maxRight, right);
+    }
+    
+    answer = maxLeft * maxRight;
+    console.log(answer);
+}
