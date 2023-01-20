93633c828fb137243a9ea42a236ac7af39d5a38b
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-20 16:36:27 +0900

    다음에 올 숫자

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-function solution(t, p) {
+function solution(common) {
     var answer = 0;
-    
-    var answer = 0;
-    
-    let n = t.length - p.length + 1;
-    for(let i = 0; i<n; i++){
-        let tmp = t.substr(i,p.length);
-        if(+tmp <= +p) answer++; 
-    }    
+    let check = false;
+    let tmp = common[1] - common[0];
+    let che = Boolean(false);
+    for(let i = common.length - 1; i > 0; i--){
+        if(tmp != common[i] - common[i - 1]) {
+            check = true;
+            continue;
+        }
+    }
+    if(check) {
+        tmp = common[1] / common[0];
+        answer = common[common.length - 1] * tmp;
+    }else {
+        answer = common[common.length - 1] + tmp;
+    }
     return answer;
 }
