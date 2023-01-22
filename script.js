51a4e88924e7002c146e2253c3271c8ce38d837a
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-22 19:33:41 +0900

    귤 모으기

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
-function solution(n, t) {
-    while(t>0){
-        n *= 2;
-        t--;
+
+
+function solution(k, tangerine){
+    let answer = 0;
+    let cntArr = {};
+    for(let i = 0; i < tangerine.length; i++){
+        if(cntArr[tangerine[i]]) {
+            cntArr[tangerine[i]]++;
+        }else{
+            cntArr[tangerine[i]] = 1;
+        } 
     }
-    return n;
+    let removeArr = Object.values(cntArr);
+
+    removeArr.sort((a, b) => b - a);
+    while(k > 0){
+        k -= removeArr[answer];
+        answer++;
+    }
+    console.log(answer);
 }
 
 
-***
-    
-- `<<`
 
-function solution(n, t) {
-    return (n << t);
-}
 
-***
-- `pow`
+function solution(k, tangerine) {
+    let answer = 0;
+    let cntArr = {};
 
-function solution(n,t) {
-    return n * Math.pow(2,t);
-}
+    for(let i = 0; i < tangerine.length; i++){
+        if(cntArr[tangerine[i]]) {
+            cntArr[tangerine[i]]++;
+        }else{
+            cntArr[tangerine[i]] = 1;
+        }
+    }
+    let removeArr = [];
+    for(let[key,value] of Object.entries(cntArr)) {
+        removeArr.push(value);
+    }
+    removeArr.sort((a, b) => b - a);
+    while(k > 0){
+        k -= removeArr[answer];
+        answer++;
+    }
 
-***
-
-- `**`
-
-function solution(n, t) {
-    return n * (2 ** (t));
+    return answer;
 }
