93e887206315706f3ef655384123569ff48a25f9
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-23 22:40:08 +0900

    약수구하기
    Math.sqrt(), Set객체

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 function solution(n) {
-    let tmp = Math.sqrt(n);
-    
-    if((tmp % Math.floor(tmp)) === 0){
-        return 1;
-    }else{
-        return 2;
+    let answer = [];
+
+    for(let i = 1; i <= Math.sqrt(n); i++){
+        let tmp = n / i;
+        if(Number.isInteger(tmp)){
+            answer.push(i);
+            if(i !== tmp){
+                answer.push(tmp);
+            }
+        }
     }
+    answer.sort((a, b) => a - b);
+    return answer;
 }
 
-// ----------------------------
-
+// Set객체 : 자료형에 관계 없이 원시값과 객체 참조 모두 유일한 값만 저장할 수 있다.
 function solution(n) {
-    let tmp = Math.sqrt(n);
-    
-    if(Number.isInteger(tmp)){
-        return 1;
-    }else{
-        return 2;
+    let answer = new Set;
+
+    for(let i = 1; i <= Math.sqrt(n); i++){
+        if(n % i === 0){
+            answer.add(i);
+            answer.add(n / i);
+        }
     }
+    return [...answer].sort((a, b) => a - b);
 }
