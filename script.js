19d69e1f65dc496c34acc27db87b437145422b64
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-24 13:39:21 +0900

    합성수 찾기

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-function solution(n){
-    let answer = [];
-
-    for(let i = 2; i <= n; i++){
-        if(n % i === 0){
-            answer.push(i);
-            n /= i;
-            while(1){
-                if(n % i !== 0){
-                    break;
-                }
-                n /= i;
-            }
+function solution(n) {
+    var answer = 0;
+    
+    let arr =  new Array(n + 1).fill(0);
+    
+    for(let i = 2; i <= Math.sqrt(n); i++){
+        if(arr[i])continue;
+        for(let j = i * 2; j <= n; j += i){
+            arr[j] = 1;
         }
     }
+    for(let i = 4; i <= n; i++){
+        if(arr[i]) answer++;
+    }
     return answer;
+}
+
+// --------------------------------
+
+function solution(n) {
+    let arr =  new Array(n + 1).fill(0);
+    
+    for(let i = 2; i <= n; i++){
+        if(!arr[i]){
+            for(let j = 2; i * j <= n; j++){
+                arr[i * j] = 1;
+            }
+        } 
+    }
+    return arr.filter(el => el===1).length;
 }
