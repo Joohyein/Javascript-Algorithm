11a21a77f0d4f74473be2a20916b281ceea1c017
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-25 09:28:25 +0900

    피보나치 수
    재귀함수, for문

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
+// 재귀함수로 풀었더니 8000부터 콜스택이 터져버렸다.
 function solution(n) {
     var answer = 0;
+    const MAX = 1234567;
     
-    let arr =  new Array(n + 1).fill(0);
-    
-    for(let i = 2; i <= Math.sqrt(n); i++){
-        if(arr[i])continue;
-        for(let j = i * 2; j <= n; j += i){
-            arr[j] = 1;
+    let dp = new Array(n + 1).fill(0);
+    function fibo(n){
+        if(n === 1 || n === 2){
+            dp[n] = 1;
+            return dp[n];
+        } else if (dp[n] === 0){
+            dp[n] = fibo(n - 1) + fibo(n - 2);
+            return dp[n] % MAX;
+        } else {
+            return dp[n] % MAX;
         }
     }
-    for(let i = 4; i <= n; i++){
-        if(arr[i]) answer++;
-    }
+    
+    answer = fibo(n);
     return answer;
 }
 
-// --------------------------------
+// 재귀함수보다 for문으로 푸는 것이 더 빠르고 콜스택에 문제가 없다.
 
 function solution(n) {
-    let arr =  new Array(n + 1).fill(0);
+    var answer = 0;
+    const MAX = 1234567;
     
-    for(let i = 2; i <= n; i++){
-        if(!arr[i]){
-            for(let j = 2; i * j <= n; j++){
-                arr[i * j] = 1;
-            }
-        } 
+    let num1 = 0;
+    let num2 = 1;
+    for(let i = 1; i < n; i++){
+        answer = (num1 + num2) % MAX;
+        num1 = num2;
+        num2 = answer;
     }
-    return arr.filter(el => el===1).length;
+    return answer;
 }
