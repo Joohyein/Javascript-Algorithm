a559af7492223103aa56bd67d27e4e8145ef1739
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-27 19:28:15 +0900

    완주하지 못한 선수

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-function solution(n) {
+function solution(participant, completion) {
     var answer = '';
-    for(let i = 0; i < n; i++){
-        if(i % 2 === 0){
-            answer += '수';
-        } else {
-            answer += '박';
+
+    for(let i = 0; i < participant.length; i++){
+        for(let j = 0; j < completion.length; j++){
+            if(participant[i] === completion[j]){
+                participant.splice(i,1);
+                completion.splice(j,1);
+                j--;
+                i--;
+            }
         }
     }
+    answer = participant[0];
+    
+    return answer;
+}
+// -----> 시간초과
+
+
+// ------------------------------------
+
+function solution(participant, completion) {
+    var answer = '';
+    participant.sort();
+    completion.sort();
+    for(let i = 0; i < completion.length; i++){
+        if(participant[i] !== completion[i]){
+            answer = participant[i];
+            break;
+        }
+    }
+    if(answer === '') answer = participant[participant.length-1];
     return answer;
 }
