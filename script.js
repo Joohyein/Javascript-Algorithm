d7468ac00c4709fdc52f3172c1a603bb03426931
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-27 19:27:16 +0900

    수박수박수박수박수박수?

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-function solution(arr, divisor) {
-    var answer = [];
-
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] % divisor === 0) answer.push(arr[i]);
+function solution(n) {
+    var answer = '';
+    for(let i = 0; i < n; i++){
+        if(i % 2 === 0){
+            answer += '수';
+        } else {
+            answer += '박';
+        }
     }
-    if(answer.length === 0) answer.push(-1);
-    answer.sort((a,b) => a - b);
     return answer;
-}
-
-// -------------------------------------
-
-function solution(arr, divisor) {
-    var answer = [];
-    arr.map((o) => {
-        o % divisor === 0 && answer.push(o);
-    })
-    return answer.length ? answer.sort((a, b) => a - b) : [-1];
-
 }
