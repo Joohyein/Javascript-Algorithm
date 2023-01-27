955e323780ecd8cb7b63437cf17eb9219d6c0b02
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-27 19:28:48 +0900

    자릿수 더하기

--- script.js
+++ script.js
@@ -1,35 +1,11 @@
-function solution(participant, completion) {
-    var answer = '';
+function solution(n)
+{
+    var answer = 0;
 
-    for(let i = 0; i < participant.length; i++){
-        for(let j = 0; j < completion.length; j++){
-            if(participant[i] === completion[j]){
-                participant.splice(i,1);
-                completion.splice(j,1);
-                j--;
-                i--;
-            }
-        }
+    while(n > 0){
+        answer += Math.floor(n % 10);
+        n /= 10;
     }
-    answer = participant[0];
-    
-    return answer;
-}
-// -----> 시간초과
 
-
-// ------------------------------------
-
-function solution(participant, completion) {
-    var answer = '';
-    participant.sort();
-    completion.sort();
-    for(let i = 0; i < completion.length; i++){
-        if(participant[i] !== completion[i]){
-            answer = participant[i];
-            break;
-        }
-    }
-    if(answer === '') answer = participant[participant.length-1];
     return answer;
 }
