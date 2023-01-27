2d03c2baf502e99c5872274de8dede871df36fbe
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-27 20:49:00 +0900

    정수 내림차순으로 배치하기

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-function solution(s, n) {
-    var answer = '';
-    let alpha = "Aa";
+function solution(n) {
+    let answer = n.toString()
+    .split('')
+    .sort((a, b) => b - a)
+    .join('');
 
-    for(let i = 0; i < s.length; i++){
-        let num = s[i].charCodeAt(0);
-        let tmp = 0;
-        if(s[i] >= 'A' && s[i] <= 'Z'){
-            tmp = Math.floor((num - alpha.charCodeAt(0) + n) % 26);
-            answer += String.fromCharCode(tmp + alpha.charCodeAt(0));
-        } 
-        else if(s[i] >= 'a' && s[i] <= 'z'){
-            tmp = Math.floor((num - alpha.charCodeAt(1) + n) % 26);
-            answer += String.fromCharCode(tmp + alpha.charCodeAt(1));
-        }
-        else answer += s[i];
-    }
-
-    return answer;
+    return +answer;
 }
