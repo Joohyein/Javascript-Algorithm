9b1b9a1adb122d8e572655f77921857359b16449
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-27 19:21:05 +0900

    저주의 숫자 3

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
-// 재귀함수로 풀었더니 8000부터 콜스택이 터져버렸다.
 function solution(n) {
     var answer = 0;
-    const MAX = 1234567;
+    let cnt = 0;
     
-    let dp = new Array(n + 1).fill(0);
-    function fibo(n){
-        if(n === 1 || n === 2){
-            dp[n] = 1;
-            return dp[n];
-        } else if (dp[n] === 0){
-            dp[n] = fibo(n - 1) + fibo(n - 2);
-            return dp[n] % MAX;
-        } else {
-            return dp[n] % MAX;
+    while(1){
+    if(cnt === n) break;
+    cnt++;
+    answer++;
+
+    let str = answer.toString();
+    while(1){
+        str = answer.toString();
+        if(answer === 3 || answer % 3 === 0 || str.includes(3)){
+            answer++;
+        } 
+        str = answer.toString();
+        if(answer !== 3 && answer % 3 !== 0 && !str.includes(3)) {
+            break;
         }
     }
-    
-    answer = fibo(n);
-    return answer;
 }
-
-// 재귀함수보다 for문으로 푸는 것이 더 빠르고 콜스택에 문제가 없다.
-
-function solution(n) {
-    var answer = 0;
-    const MAX = 1234567;
-    
-    let num1 = 0;
-    let num2 = 1;
-    for(let i = 1; i < n; i++){
-        answer = (num1 + num2) % MAX;
-        num1 = num2;
-        num2 = answer;
-    }
     return answer;
 }
