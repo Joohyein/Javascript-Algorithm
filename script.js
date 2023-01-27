6d808882b1272b3c6aa5306b4355ae648baac0d6
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-27 19:21:41 +0900

    2016

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-function solution(n) {
-    var answer = 0;
-    let cnt = 0;
-    
+function solution(a, b) {
+    var answer = '';
+    let arr = ['FRI', 'SAT', 'SUN', 'MON',  'TUE', 'WED', 'THU'];
+    let cnt = 1;
+    let tmp = 0;
     while(1){
-    if(cnt === n) break;
-    cnt++;
-    answer++;
-
-    let str = answer.toString();
-    while(1){
-        str = answer.toString();
-        if(answer === 3 || answer % 3 === 0 || str.includes(3)){
-            answer++;
-        } 
-        str = answer.toString();
-        if(answer !== 3 && answer % 3 !== 0 && !str.includes(3)) {
-            break;
+        if(cnt === a) break;
+        if(cnt === 1 || cnt === 3 || cnt === 5 || cnt === 7 || cnt === 8 || cnt === 10 || cnt ===12){
+            tmp += 31;
         }
+        else if(cnt === 2) tmp += 29;
+        else tmp += 30;
+        cnt++;
     }
-}
+    
+    tmp += b - 1;
+
+    answer = arr[tmp % 7];
     return answer;
 }
