85e2bae7e58aaa88e4e66158b4479b67bda715a9
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-28 09:58:38 +0900

    최빈값 구하기
    filter

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-function solution(n) {
-    let answer = n.toString()
-    .split('')
-    .sort((a, b) => b - a)
-    .join('');
+function solution(array) {
+    let a = array.filter((num, index) => array.indexOf(num) == index); 
+    if(a.length === 1) return array[0];
+    let arr = {};
 
-    return +answer;
+    for(let i = 0; i < array.length; i++){
+        if(arr[array[i]]) arr[array[i]]++;
+        else arr[array[i]] = 1;
+    }
+    
+    let tmp = [];
+    for(let value in arr){
+        tmp.push([value, arr[value]]);
+    }
+    
+    tmp.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+    
+    if(tmp[0][1] === tmp[1][1]) return -1;
+    return +tmp[0][0];
 }
