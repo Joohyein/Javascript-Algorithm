47d7d0afab08858430c12d9478bebe4e54a65ae4
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-28 22:16:21 +0900

    같은 숫자는 싫어

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-function solution(sizes) {
-    var answer = 0;
+function solution(arr)
+{
+    var answer = [arr[0]];
     
-    let left = 0;
-    let right = 0;
-    let maxLeft = 0;
-    let maxRight = 0;
-    
-    for(let i = 0; i < sizes.length; i++){
-        if(sizes[i][0] > sizes[i][1]){
-            left = sizes[i][0];
-            right = sizes[i][1];
-        }else {
-            left = sizes[i][1];
-            right = sizes[i][0];
+    let tmp = arr[0];
+    for(let i = 0; i < arr.length; i++){
+        if(tmp !== arr[i]) {
+            answer.push(arr[i])
+            tmp = arr[i];
         }
-        maxLeft = Math.max(maxLeft, left);
-        maxRight = Math.max(maxRight, right);
     }
     
-    answer = maxLeft * maxRight;
+    return answer;
+}
+
+// -------------------------------
+
+// filter
+function solution(arr){
+    answer = arr.filter((num, index) => num != arr[index + 1])
     return answer;
 }
