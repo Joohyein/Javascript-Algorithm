ca5cdfe85fbc0920635226907880f0e58417e6f3
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-28 21:49:07 +0900

    정수 제곱근 판별

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-function solution(array) {
-    let a = array.filter((num, index) => array.indexOf(num) == index); 
-    if(a.length === 1) return array[0];
-    let arr = {};
+function solution(n){
+    let answer = 0;
+    let root = Math.sqrt(n)
 
-    for(let i = 0; i < array.length; i++){
-        if(arr[array[i]]) arr[array[i]]++;
-        else arr[array[i]] = 1;
-    }
-    
-    let tmp = [];
-    for(let value in arr){
-        tmp.push([value, arr[value]]);
-    }
-    
-    tmp.sort(function(a, b) {
-        return b[1] - a[1];
-    });
-    
-    if(tmp[0][1] === tmp[1][1]) return -1;
-    return +tmp[0][0];
+    if(Number.isInteger(root)) answer = (root + 1) ** 2;
+    else answer = -1;
+
+    return answer;
 }
