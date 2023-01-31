d219a1317e9442faefec743460d1eb04165ce461
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-31 11:01:34 +0900

    시저 암호
    charCodeAt()

--- script.js
+++ script.js
@@ -1,57 +1,23 @@
-function solution(s) {
+function solution(s, n) {
     var answer = '';
+    let alpha = "Aa";
 
     for(let i = 0; i < s.length; i++){
-        if(s.substr(i, 4) === "zero") {
-            answer += 0;
-            i += 3;
+        let num = s[i].charCodeAt(0);
+        let tmp = 0;
+        if(s[i] >= 'A' && s[i] <= 'Z'){
+            tmp = Math.floor((num - alpha.charCodeAt(0) + n) % 26);
+            answer += String.fromCharCode(tmp + alpha.charCodeAt(0));
         } 
-        else if(s.substr(i, 3) === "one") {
-            answer += 1;
-            i += 2;
-        }else if(s.substr(i, 3) === "two") {
-            answer += 2;
-            i += 2;
-        }else if(s.substr(i, 5) === "three") {
-            answer += 3;
-            i += 4;
-        }else if(s.substr(i, 4) === "four") {
-            answer += 4;
-            i += 3;
-        }else if(s.substr(i, 4) === "five") {
-            answer += 5;
-            i += 4;
-        }else if(s.substr(i, 3) === "six") {
-            answer += 6;
-            i += 2;
-        }else if(s.substr(i, 5) === "seven") {
-            answer += 7;
-            i += 4;
-        }else if(s.substr(i, 5) === "eight") {
-            answer += 8;
-            i += 4;
-        }else if(s.substr(i, 4) === "nine") {
-            answer += 9;
-            i += 3;
-        } else {
-            answer += s[i];
+        else if(s[i] >= 'a' && s[i] <= 'z'){
+            tmp = Math.floor((num - alpha.charCodeAt(1) + n) % 26);
+            answer += String.fromCharCode(tmp + alpha.charCodeAt(1));
         }
+        else answer += s[i];
     }
 
-    return +answer;
+    return answer;
 }
 
-//-------------------------------------------
-// join(n)
-function solution(s) {
-    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    var answer = s;
-
-    for(let i=0; i< numbers.length; i++) {
-        let arr = answer.split(numbers[i]);
-        console.log(arr);
-        answer = arr.join(i);
-    }
-
-    return Number(answer);
-}
+//charAt 는 index에 해당하는 문자를 리턴
+//chartCodeAt은 유니코드 값을 리턴
