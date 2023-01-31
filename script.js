9b7d815381120c1718c70ace79972c7be5d0da00
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-31 10:56:53 +0900

    숫자 문자열과 영단어
    i값으로 연결시켜주는 join 사용

--- script.js
+++ script.js
@@ -1,42 +1,57 @@
-function solution(nums) {
-    var answer = -1;
-    
-    // 소수 찾기
-    let check = new Array(3001).fill(true);
-    check[0] = false;
-    check[1] = false;
-    for(let i = 2; i <= Math.sqrt(3000) ; i++){
-        if(!check[i]) continue;
-        for(let j = i * 2; j <= 3000; j += i){
-            check[j] = false;
+function solution(s) {
+    var answer = '';
+
+    for(let i = 0; i < s.length; i++){
+        if(s.substr(i, 4) === "zero") {
+            answer += 0;
+            i += 3;
+        } 
+        else if(s.substr(i, 3) === "one") {
+            answer += 1;
+            i += 2;
+        }else if(s.substr(i, 3) === "two") {
+            answer += 2;
+            i += 2;
+        }else if(s.substr(i, 5) === "three") {
+            answer += 3;
+            i += 4;
+        }else if(s.substr(i, 4) === "four") {
+            answer += 4;
+            i += 3;
+        }else if(s.substr(i, 4) === "five") {
+            answer += 5;
+            i += 4;
+        }else if(s.substr(i, 3) === "six") {
+            answer += 6;
+            i += 2;
+        }else if(s.substr(i, 5) === "seven") {
+            answer += 7;
+            i += 4;
+        }else if(s.substr(i, 5) === "eight") {
+            answer += 8;
+            i += 4;
+        }else if(s.substr(i, 4) === "nine") {
+            answer += 9;
+            i += 3;
+        } else {
+            answer += s[i];
         }
     }
-    let arr = [];
-    let cnt = 0;
-    let visited = new Array(nums.length + 1).fill(false);
-    function solve(num, x){
-        if(x === 3){
-            let sum = nums[arr[0]] + nums[arr[1]] + nums[arr[2]];
-            if(check[sum]) {
-	              cnt++;
-	              check[sum] = false;
-	          }
-        }
-        else {
-            for(let i = num; i < nums.length; i++){
-                if(visited[i]) continue;
-                visited[i] = true;
-                arr.push(i);
-                solve(i + 1, x + 1);
-                arr.pop();
-                visited[i] = false;
-            }
-        }
-    }
-    solve(0, 0);
-    
-    if(cnt !== 0) return cnt;
-    return answer;
+
+    return +answer;
 }
 
+//-------------------------------------------
+// join(n)
+function solution(s) {
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    var answer = s;
 
+    for(let i=0; i< numbers.length; i++) {
+        let arr = answer.split(numbers[i]);
+        console.log(arr);
+        answer = arr.join(i);
+    }
+
+    return Number(answer);
+}
