12b24e6dc7c061f33fcf06f05d8b6b3ab572efef
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-31 11:02:48 +0900

    약수의 개수와 덧셈

--- script.js
+++ script.js
@@ -1,42 +1,15 @@
-function solution(new_id) {
-    var answer = '';
+function solution(left, right) {
+    var answer = 0;
 
-    new_id = new_id.toLowerCase();  // 1단계
 
-    new_id = new_id.split('');
-    for(let i = 0; i < new_id.length; i++){
-        if(new_id[i]>='a' && new_id[i]<='z') continue;
-        if(new_id[i] === '-' || new_id[i]==='_' || new_id[i]==='.') continue;
-        if(new_id[i] >= '0' && new_id[i] <= '9') continue;
-        new_id.splice(i, 1);
-        i--;
-    } // 2단계
-
-    for(let i = 0; i < new_id.length - 1; i++){
-        if(new_id[i] === '.'){
-            if(new_id[i + 1] === '.'){
-                new_id.splice(i, 1);
-                i--;
-            }
+    for(let i = left; i <= right; i++){
+        let cnt = 0;
+        for(let j = 1; j <= i; j++){
+            if(i % j === 0) cnt++;
         }
-    } // 3단계
+        if(cnt % 2 === 0) answer += i;
+        else answer -= i;
+    }
 
-    if(new_id[0] === '.') new_id.shift();
-    if(new_id[new_id.length - 1] === '.') new_id.pop(); // 4단계
-
-    if(new_id.length === 0) new_id.push('a'); // 5단계
-    if(new_id.length > 15) new_id.splice(15, new_id.length - 15); // 6단계
-
-    if(new_id[new_id.length - 1] === '.') new_id.pop(); // 4단계
-
-
-    if(new_id.length < 3) {
-        let tmp = new_id[new_id.length - 1];
-        while(new_id.length < 3){
-            new_id.push(tmp);
-        }
-    } // 7단계
-
-    answer = new_id.join('');
     return answer;
 }
