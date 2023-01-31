12d420875f85a3383c3af649de42d97835444b45
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-31 23:10:59 +0900

    실패율

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-function solution(n) {
-    var answer = 0;
-    for(let i = 2; i < n; i++){
-        if(n % i === 1) return i;
+function solution(N, stages) {
+    var answer = [];
+
+    let cntArr = {};
+    for(let i = 1; i <= N; i++){
+        cntArr[i] = 0;
     }
+
+    for(let i = 0; i < stages.length; i++){
+        if(cntArr[stages[i]]) cntArr[stages[i]]++;
+        else cntArr[stages[i]] = 1;
+    }
+    console.log(cntArr);
+    let arr = [];
+    let person = stages.length;
+    for(let tmp in cntArr){
+        console.log(person);
+        arr.push([tmp, cntArr[tmp]/person]);
+        person -= cntArr[tmp];
+
+    }
+    arr = arr.sort((a, b) => {
+        if(a[1] === b[1]) return a - b;
+        return b[1] - a[1];
+    });
+
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i][0] > N) continue;
+        answer.push(+arr[i][0]);
+    }
+    return answer;
 }
