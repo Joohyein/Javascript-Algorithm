5943c5774a73903c7846a767a6a5a3ec55297706
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-31 11:03:04 +0900

    약수의 합

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-function solution(left, right) {
+function solution(n) {
     var answer = 0;
-
-
-    for(let i = left; i <= right; i++){
-        let cnt = 0;
-        for(let j = 1; j <= i; j++){
-            if(i % j === 0) cnt++;
-        }
-        if(cnt % 2 === 0) answer += i;
-        else answer -= i;
+    
+    for(let i = 1; i <= n; i++){
+        if(n % i === 0) answer += i;
     }
-
+    
     return answer;
 }
