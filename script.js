64aa8fe6f5f97296ae56a050ad36ca63061300a9
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-01-31 11:03:18 +0900

    예산

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-function solution(n) {
+function solution(d, budget) {
     var answer = 0;
     
-    for(let i = 1; i <= n; i++){
-        if(n % i === 0) answer += i;
+    d = d.sort((a, b) => a - b);
+
+    let tmp = 0;
+    while(answer < d.length){
+        tmp += d[answer];
+        if(tmp > budget) break;
+        answer++;
     }
-    
+
     return answer;
 }
