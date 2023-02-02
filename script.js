cf2790d679550103e0689bd4a3916a80d00ae07a
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-02-02 09:50:16 +0900

    백준 - #1110 더하기 사이클

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
-function solution(board, moves) {
-    var answer = 0;
+const fs = require('fs');
+const inputData = fs.readFileSync('/dev/stdin').toString().split(' ');
 
-    let arr = [];
-    for(let i = 0; i < moves.length; i++){
-        let pickNum = moves[i] - 1;
-        for(let j = 0; j < board.length; j++){
-            if(board[j][pickNum]){
-                if(arr.length > 0){
-                    if(arr[arr.length - 1] === board[j][pickNum]){
-                        arr.pop();
-                        answer += 2;
-                    }
-                    else arr.push(board[j][pickNum]);
-                }
-                else arr.push(board[j][pickNum]);
-                board[j][pickNum] = 0;
-                break;
-            }
-        }
-    }
-
-    return answer;
+let n = inputData;
+let checkNum = inputData;
+let cnt = 0;
+while(1){
+    cnt++;
+    let sum = Math.floor((n % 10) + (n / 10)); // 8
+    n = (n % 10 * 10) + (sum % 10);
+    if(n == checkNum) break;
 }
+console.log(cnt);
